1c630aae4a6b77c81e7228b1f61b0a15ccda4a7a
Vamshi Epuri <a167250@pm.local>
2021-12-04 01:27:32 -0600

    init commit

---
+++ src/main/java/com/example/employeemanagementapiangularoauth2/EmployeeManagementApiAngularOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementapiangularoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiAngularOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApiAngularOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeemanagementapiangularoauth2/EmployeeManagementApiAngularOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementapiangularoauth2;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApiAngularOauth2ApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
